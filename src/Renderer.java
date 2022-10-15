69068878d023b4cec573e871cc4c157383399688
sakacj <79791046+sakacj@users.noreply.github.com>
2022-10-15 15:35:52 +0200

    update

--- src/Renderer.java
+++ src/Renderer.java
@@ -1,4 +1,5 @@
 import lwjglutils.OGLBuffers;
+import lwjglutils.OGLTextRenderer;
 import lwjglutils.OGLTexture2D;
 import lwjglutils.ShaderUtils;
 import transforms.Camera;
@@ -6,6 +7,9 @@ import transforms.Mat4;
 import transforms.Mat4PerspRH;
 import transforms.Vec3D;
 
+import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.IOException;
 
 import static org.lwjgl.opengl.GL33.*;
@@ -16,7 +20,16 @@ public class Renderer {
 
     private Camera camera;
     private Mat4 projection;
+
+
     private OGLTexture2D texture;
+
+    private boolean orthoProjection = false;
+    private JPanel panel;
+    private OGLBuffers buff1;
+    private OGLBuffers buff2;
+    private OGLTextRenderer oglTextRenderer;
+
 
     public Renderer() {
         //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -40,13 +53,28 @@ public class Renderer {
         int loc_uProj = glGetUniformLocation(shaderProgram, "u_Proj");
         glUniformMatrix4fv(loc_uProj, false, projection.floatArray());
 
-        grid = new Grid(20, 20);
+
 
         try {
             texture = new OGLTexture2D("./textures/bricks.jpg");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+
+        //
+        grid = new Grid();
+        buff1 = Grid.gridListTriangle(10, 10);
+        buff2 = Grid.gridStripsTriangle(5, 5);
+
+
+        oglTextRenderer = new OGLTextRenderer(600, 800);
+        oglTextRenderer.addStr2D(10, 10, "Sakač");
+
+
+        panel.addMouseListener();
+
+
     }
 
     public void draw() {
