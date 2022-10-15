78692217e1d427aaef443747553bbf02c2cc34ad
sakacj <79791046+sakacj@users.noreply.github.com>
2022-10-15 14:18:50 +0200

    cv4 + update

--- src/Renderer.java
+++ src/Renderer.java
@@ -1,48 +1,57 @@
 import lwjglutils.OGLBuffers;
+import lwjglutils.OGLTexture2D;
 import lwjglutils.ShaderUtils;
 import transforms.Camera;
 import transforms.Mat4;
 import transforms.Mat4PerspRH;
 import transforms.Vec3D;
 
+import java.io.IOException;
+
 import static org.lwjgl.opengl.GL33.*;
 
 public class Renderer {
-
     private int shaderProgram;
-
     private Grid grid;
 
     private Camera camera;
     private Mat4 projection;
+    private OGLTexture2D texture;
 
     public Renderer() {
-        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
         camera = new Camera()
-                .withPosition(new Vec3D(0.5f,-2f,1.5f))
+                .withPosition(new Vec3D(0.5f, -2f, 1.5f))
                 .withAzimuth(Math.toRadians(90))
                 .withZenith(Math.toRadians(-45));
-
-        projection = new Mat4PerspRH(Math.PI/3,600/(float)800, 0.1f,50.f);
+        projection = new Mat4PerspRH(Math.PI / 3, 600 / (float)800, 0.1f, 50.f);
 
         shaderProgram = ShaderUtils.loadProgram("/shaders/Basic");
         glUseProgram(shaderProgram);
 
+        // Color
         int loc_uColorR = glGetUniformLocation(shaderProgram, "u_ColorR");
         glUniform1f(loc_uColorR, 1.f);
-
+        // View
         int loc_uView = glGetUniformLocation(shaderProgram, "u_View");
-        glUniformMatrix2x4fv(loc_uView, false, projection.floatArray());
-
+        glUniformMatrix4fv(loc_uView, false, camera.getViewMatrix().floatArray());
+        // Proj
         int loc_uProj = glGetUniformLocation(shaderProgram, "u_Proj");
-        glUniformMatrix2x4fv(loc_uProj, false, camera.getViewMatrix().floatArray());
+        glUniformMatrix4fv(loc_uProj, false, projection.floatArray());
 
         grid = new Grid(20, 20);
 
+        try {
+            texture = new OGLTexture2D("./textures/bricks.jpg");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void draw() {
+        texture.bind();
         grid.getBuffers().draw(GL_TRIANGLES, shaderProgram);
     }
 }
+
